c854c5cc317d11dffb5afe08e39901cd19bf0e58
Izentropowy <119621854+Izentropowy@users.noreply.github.com>
2023-04-10 23:36:57 +0200

    Add functionality to the tool buttons

--- script.js
+++ script.js
@@ -1,26 +1,20 @@
 const grid = document.querySelector(".grid");
 const gridSide = grid.clientHeight;
+const blackButton = document.querySelector(".btn-black");
+const randomButton = document.querySelector(".btn-random");
+const eraseButton = document.querySelector(".btn-erase");
+const clearButton = document.querySelector(".btn-clear");
 var pixel;
+let markerColor = "black";
 
-;// check marker mode (black default)
 
-// change marker to black
-
-// change marker to random
-
-// change marker to white
-
-// erase the board
-
-// Build a grid (120/80/60/48/40/24/20)
-
+// Build a grid (80/60/48/40/24/20)
 function buildGrid(resolution){
     let a = gridSide / resolution;
     for (let i = 0; i < resolution; i++){
         for (let j = 0; j < resolution; j++){
             pixel = document.createElement("div");
             grid.appendChild(pixel);
-            pixel.style.backgroundColor = "red";
             pixel.style.width = `${a}px`;
             pixel.style.height = `${a}px`;
         }
@@ -30,7 +24,6 @@ function buildGrid(resolution){
 buildGrid(60);
 
 // check if mouse is currently clicked
-
 var mouseDown = 0;
 document.body.onmousedown = function() { 
   ++mouseDown;
@@ -40,24 +33,47 @@ document.body.onmouseup = function() {
 }
 
 // this must be done after the grid is built (!)
-
 const gridChildren = Array.from(grid.children)
 
 // color pixels only if mouse over AND mouse clicked
-
 gridChildren.forEach(function(element){
     element.addEventListener('mouseover', function(){
         if(mouseDown){
-            element.style.backgroundColor = "black";
+            element.style.backgroundColor = markerColor;
         }
     })
 
     // first pixel clicked should also be colored
-
     element.addEventListener('click', function(){
-        element.style.backgroundColor = "black";
+        element.style.backgroundColor = markerColor;
     })
 })
+
+// change marker to black
+blackButton.addEventListener('click', function(){
+    markerColor = "black";
+})
+
+// change marker to random
+randomButton.addEventListener('click', function(){
+    let R = Math.floor(Math.random() * 256);
+    let G = Math.floor(Math.random() * 256);
+    let B = Math.floor(Math.random() * 256);
+    markerColor = `rgb(${R}, ${G}, ${B})`;
+})
+
+// change marker to white
+eraseButton.addEventListener('click', function(){
+    markerColor = "white";
+})
+
+// erase the board
+clearButton.addEventListener('click', function(){
+    gridChildren.forEach(function(element){
+        element.style.backgroundColor = "white";
+    })
+})
+
 
 
 
