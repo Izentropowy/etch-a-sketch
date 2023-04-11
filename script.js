cb60723210d22853218a1c4557ab1f42c0625b5b
Izentropowy <119621854+Izentropowy@users.noreply.github.com>
2023-04-11 15:57:15 +0200

    Zoom buttons when hover

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 const grid = document.querySelector(".grid");
-const gridSide = grid.clientHeight;
+var gridSide = grid.clientHeight;
 const blackButton = document.querySelector(".btn-black");
 const randomButton = document.querySelector(".btn-random");
 const eraseButton = document.querySelector(".btn-erase");
 const clearButton = document.querySelector(".btn-clear");
 const label = document.querySelector("label");
 const slider = document.getElementById("resolution");
+var gridChildren;
 var pixel;
 let markerColor = "black";
+const resolution = 80;
 
-// Build a grid (80/60/40/20)
 function buildGrid(resolution){
     let a = gridSide / resolution;
     for (let i = 0; i < resolution; i++){
@@ -23,33 +24,24 @@ function buildGrid(resolution){
     }
 }
 
-buildGrid(60);
+function draw(){
+    // this must be done after the grid is built (!)
+    gridChildren = Array.from(grid.children)
 
-// check if mouse is currently clicked
-var mouseDown = 0;
-document.body.onmousedown = function() { 
-  ++mouseDown;
-}
-document.body.onmouseup = function() {
-  --mouseDown;
-}
+    // color pixels only if mouse over AND mouse clicked
+    gridChildren.forEach(function(element){
+        element.addEventListener('mouseover', function(){
+            if(mouseDown){
+                element.style.backgroundColor = markerColor;
+            }
+        })
 
-// this must be done after the grid is built (!)
-const gridChildren = Array.from(grid.children)
-
-// color pixels only if mouse over AND mouse clicked
-gridChildren.forEach(function(element){
-    element.addEventListener('mouseover', function(){
-        if(mouseDown){
+        // first pixel clicked should also be colored
+        element.addEventListener('click', function(){
             element.style.backgroundColor = markerColor;
-        }
+        })
     })
-
-    // first pixel clicked should also be colored
-    element.addEventListener('click', function(){
-        element.style.backgroundColor = markerColor;
-    })
-})
+}
 
 // tools functionality
 blackButton.addEventListener('click', function(){
@@ -78,9 +70,25 @@ clearButton.addEventListener('click', function(){
 
 slider.addEventListener('input', function(){
     label.textContent = this.value + ' x ' + this.value;
-    // grid.innerHTML = '';
+    grid.innerHTML = '';
     buildGrid(this.value);
+    draw();
 })
+
+// check if mouse is currently clicked
+var mouseDown = 0;
+document.body.onmousedown = function() { 
+++mouseDown;
+}
+document.body.onmouseup = function() {
+--mouseDown;
+}
+
+
+buildGrid(resolution);
+draw();
+
+
 
 
 
